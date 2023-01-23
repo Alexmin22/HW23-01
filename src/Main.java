ec54970522566434bb14bb5e14986ab7ea7507c8
Alexmin22 <yashi80@list.ru>
2023-01-23 21:04:07 +0300

    Здесь 4 задачи на одном листе. Сделал через вложенные try/catch

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+    //1.Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+    //2.Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+    //3.Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+    //4.Перехватить исключение (и вывести его на экран), указав его тип, возникающее при выполнении кода:
+    public static void main(String[] args) throws Throwable {
+        try {
+            int a = 42 / 0;
+            System.out.println(a);
+            try {
+                String s = null;
+                s.toUpperCase();
+                try {
+                    ArrayList<String> list = new ArrayList<>();
+                    String st = list.get(18);
+                    try {
+                        HashMap<String, String> map = new HashMap<>(null);
+                        map.put(null, null);
+                        map.remove(null);
+                    } catch (NullPointerException np) {
+                        throw new Throwable(np.getMessage());
+                    }
+                } catch (IndexOutOfBoundsException inEx) {
+                    throw new RuntimeException().fillInStackTrace();
+                }
+                } catch (NullPointerException npe) {
+                throw new NullPointerException("Проблема со строкой S");
+            }
+            } catch (ArithmeticException e) {
+            throw new RuntimeException(e.toString());
+        }
+    }
+}
